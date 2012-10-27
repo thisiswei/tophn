b9017dd8e695da95887cbd3cefccc4c8ff4803f0
thisiswei <99plus2@gmail.com>
2012-10-27 10:01:18 -0700

    bootup..

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linksgram::Application.config.secret_token = '52e1a51e37738d2bd50935a9080c7f7f6ed66ae1f9319677aa6205a67d13b5881c0b15a8f1e50510cef84d4651a2ccec3e5f2a0b47640f67ca88b4beca5a9cc2'
