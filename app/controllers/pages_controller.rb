9ae69eaa9d9e016d051a22e7a0d1779de885e375
thisiswei <99plus2@gmail.com>
2012-11-12 11:23:05 -0800

    ..

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
 
   def index
-    @top_links = Link.all.sort_by{|l| l.votes.count}.reverse
-
-    @links = @top_links
+    @links = Link.all.sort_by{|l| l.votes.count}.reverse
     @user = current_user
-    #.paginate(:page => params[:page], :per_page => 30)
   end
 end
