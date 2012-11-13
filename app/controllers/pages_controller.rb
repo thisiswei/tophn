c65483d244d5439176e7b0c6f31205186488fd7f
thisiswei <99plus2@gmail.com>
2012-11-12 21:32:56 -0800

    nov/12/12

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
   def index
     Link.update
     @links = Link.paginate(:page => params[:page], :per_page => 43).order('created_at DESC')
-    @user = current_user
+    @user  = current_user
   end
 end
