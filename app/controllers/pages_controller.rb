67b476d04804ab770d1829ff4b2997d96144dfdd
thisiswei <99plus2@gmail.com>
2012-11-12 21:59:34 -0800

    how to auto update data?? nov/12/12

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def index
-    Link.update
+    Link.update_links
     @links = Link.paginate(:page => params[:page], :per_page => 43).order('created_at DESC')
     @user  = current_user
   end
