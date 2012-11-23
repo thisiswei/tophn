566df6269decd8e789a498e2ad12654bb6bc92f5
thisiswei <99plus2@gmail.com>
2012-11-22 22:34:40 -0800

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   respond_to :json, :html, :xml
   def index
     @links = Link.order('created_at DESC').page(params[:page]).per_page(Link::PER_PAGE)
-    @last_link_time = Link.last.updated_at
+    @last_link_time = Link.order('updated_at DESC').first.updated_at 
     respond_with @links
   end
 end
