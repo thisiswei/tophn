f2eed3aaab6a4a6793dc3d5b74cbf664cff64518
thisiswei <99plus2@gmail.com>
2012-11-23 20:29:10 -0800

    ping heroapp not heroku

--- lib/ping.rb
+++ lib/ping.rb
@@ -3,7 +3,7 @@ require 'net/http'
 class Ping
   class << self
     def perform
-      host = 'tophn.heroku.com'
+      host = 'tophn.herokuapp.com'
       url  = Rails.application.routes.url_helpers.root_url(host: host)
       Net::HTTP.get_response(URI.parse(url)) 
     end
