5f1b9b9b1a608f0ef66eeb20f89df074f668c26a
thisiswei <99plus2@gmail.com>
2012-11-24 12:59:02 -0800

    stop create person for a bit

--- lib/collecting_showhn.rb
+++ lib/collecting_showhn.rb
@@ -10,20 +10,20 @@ class Showhn
     private
 
       def collect_links
-        Entry.all(1).each do |entry| 
+        Entry.all(9).each do |entry| 
           title    = entry.link.title 
           if title.match(KEYWORD) 
               username = entry.user.name
               score    = entry.voting.score 
               url      = entry.link.href
               unless Link.exists?(title)
-                  person = Person.new(hn_username: username)
-                  if person.new_record?
-                    person.save!
-                  else
-                    person = Person.find_by_name(username) 
-                  end
-                  person.links.create!(title: title, hnscore: score, url: url, created_at: entry.time) 
+                 #person = Person.new(hn_username: username)
+                 #if person.new_record?
+                 #   person.save!
+                 # else
+                 #   person = Person.find_by_name(username) 
+                 # end
+                 Link.create!(title: title, hnscore: score, url: url, created_at: entry.time, hnuser: username)
               else
                 link = Link.find_by_title(title)
                 link.update_attributes(hnscore: score) 
