4cf6017068c6f89d95e2dda2cf1c638003b03c34
thisiswei <99plus2@gmail.com>
2012-11-26 18:30:20 -0800

    only sleep when creating person

--- app/models/person.rb
+++ app/models/person.rb
@@ -23,6 +23,7 @@ class Person < ActiveRecord::Base
       return if response == "No such user."
       parsed_response = Nokogiri::HTML(response)
       parsed_response.css('tr')[-3].css('td')[1].text 
+      sleep 1.5
     end    
 
     def  whether_a_alpha_geek
